66f2d1fe215325ff6e086b5c41f986ac5aa1bcdd
Sébastien Ros <sebastienros@gmail.com>
2021-11-09 07:54:16 -0800

    Test LocalFolderPath

--- Program.cs
+++ Program.cs
@@ -1,21 +1,28 @@
 using System.Reflection;
 
-Console.WriteLine($"Type.GetType(\"Windows.Storage.ApplicationData, Windows, ContentType=WindowsRuntime\"): {Type.GetType("Windows.Storage.ApplicationData, Windows, ContentType=WindowsRuntime")}");
-Console.WriteLine($"Type.GetType(\"Windows.Storage.ApplicationData, Microsoft.Windows.SDK.NET\"): {Type.GetType("Windows.Storage.ApplicationData, Microsoft.Windows.SDK.NET")}");
+if (ApplicationDataHelper.CurrentApplicationData != null)
+{
+    Console.WriteLine("ApplicationDataHelper.CurrentApplicationData is not null");
+    Console.WriteLine($"ApplicationDataHelper.LocalFolderPath: {ApplicationDataHelper.LocalFolderPath}");
+    Console.WriteLine($"ApplicationDataHelper.TemporaryFolderPath: {ApplicationDataHelper.TemporaryFolderPath}");
+}
 
-var propertyName = "TemporaryFolder";
-var CurrentApplicationData = ApplicationDataHelper.CurrentApplicationData;
-var appDataType = CurrentApplicationData?.GetType();
+// Console.WriteLine($"Type.GetType(\"Windows.Storage.ApplicationData, Windows, ContentType=WindowsRuntime\"): {Type.GetType("Windows.Storage.ApplicationData, Windows, ContentType=WindowsRuntime")}");
+// Console.WriteLine($"Type.GetType(\"Windows.Storage.ApplicationData, Microsoft.Windows.SDK.NET\"): {Type.GetType("Windows.Storage.ApplicationData, Microsoft.Windows.SDK.NET")}");
 
-Console.WriteLine($"appDataType: {appDataType}");
-Console.WriteLine($"appDataType?.GetRuntimeProperty(propertyName): {appDataType?.GetRuntimeProperty(propertyName)}");
-Console.WriteLine($"appDataType?.GetRuntimeProperty(propertyName)!.GetValue(CurrentApplicationData): {appDataType?.GetRuntimeProperty(propertyName)!.GetValue(CurrentApplicationData)}");
+// var propertyName = "TemporaryFolder";
+// var CurrentApplicationData = ApplicationDataHelper.CurrentApplicationData;
+// var appDataType = CurrentApplicationData?.GetType();
 
-var temporaryFolder = appDataType?.GetRuntimeProperty(propertyName)!.GetValue(CurrentApplicationData);
+// Console.WriteLine($"appDataType: {appDataType}");
+// Console.WriteLine($"appDataType?.GetRuntimeProperty(propertyName): {appDataType?.GetRuntimeProperty(propertyName)}");
+// Console.WriteLine($"appDataType?.GetRuntimeProperty(propertyName)!.GetValue(CurrentApplicationData): {appDataType?.GetRuntimeProperty(propertyName)!.GetValue(CurrentApplicationData)}");
 
-Console.WriteLine($"temporaryFolder?.GetType(): {temporaryFolder?.GetType()}");
-Console.WriteLine($"temporaryFolder?.GetType().GetRuntimeProperty(\"Path\"): {temporaryFolder?.GetType().GetRuntimeProperty("Path")}");
-Console.WriteLine($"temporaryFolder?.GetType().GetRuntimeProperty(\"Path\")!.GetValue(temporaryFolder): {temporaryFolder?.GetType().GetRuntimeProperty("Path")!.GetValue(temporaryFolder)}");
+// var temporaryFolder = appDataType?.GetRuntimeProperty(propertyName)!.GetValue(CurrentApplicationData);
+
+// Console.WriteLine($"temporaryFolder?.GetType(): {temporaryFolder?.GetType()}");
+// Console.WriteLine($"temporaryFolder?.GetType().GetRuntimeProperty(\"Path\"): {temporaryFolder?.GetType().GetRuntimeProperty("Path")}");
+// Console.WriteLine($"temporaryFolder?.GetType().GetRuntimeProperty(\"Path\")!.GetValue(temporaryFolder): {temporaryFolder?.GetType().GetRuntimeProperty("Path")!.GetValue(temporaryFolder)}");
 
 // Console.WriteLine(ApplicationDataHelper.CurrentApplicationData);
 // Console.WriteLine(ApplicationDataHelper.TemporaryFolderPath);
