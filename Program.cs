41def4a9a7588094f4c184d5340f0e15c02ecbaf
Sébastien Ros <sebastienros@gmail.com>
2021-11-09 07:18:01 -0800

    Create files

---
+++ Program.cs
@@ -0,0 +1,19 @@
+[assembly: IgnoresAccessChecksTo("SqliteTest")]
+
+using Microsoft.Data.Sqlite;
+using System.Runtime.CompilerServices;
+
+Console.WriteLine(Microsoft.Data.Sqlite.ApplicationDataHelper.CurrentApplicationData);
+Console.WriteLine(Microsoft.Data.Sqlite.ApplicationDataHelper.TemporaryFolderPath);
+Console.WriteLine(Microsoft.Data.Sqlite.ApplicationDataHelper.LocalFolderPath);
+
+[AttributeUsage(AttributeTargets.Assembly, AllowMultiple = true)]
+public class IgnoresAccessChecksToAttribute : Attribute
+{
+    public IgnoresAccessChecksToAttribute(string assemblyName)
+    {
+        AssemblyName = assemblyName;
+    }
+
+    public string AssemblyName { get; }
+}
