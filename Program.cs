d1b9e7636bdd0cdc138647181e88d214d5c01445
Sébastien Ros <sebastienros@gmail.com>
2021-11-09 07:35:13 -0800

    debug code

--- Program.cs
+++ Program.cs
@@ -1,4 +1,17 @@
-Console.WriteLine(ApplicationDataHelper.CurrentApplicationData);
-Console.WriteLine(ApplicationDataHelper.TemporaryFolderPath);
-Console.WriteLine(ApplicationDataHelper.LocalFolderPath);
+var propertyName = "TemporaryFolder";
+var appDataType = ApplicationDataHelper.CurrentApplicationData?.GetType();
+
+Console.WriteLine($"appDataType: {appDataType}");
+Console.WriteLine($"appDataType?.GetRuntimeProperty(propertyName): {appDataType?.GetRuntimeProperty(propertyName)}");
+Console.WriteLine($"appDataType?.GetRuntimeProperty(propertyName)!.GetValue(CurrentApplicationData): {appDataType?.GetRuntimeProperty(propertyName)!.GetValue(CurrentApplicationData)}");
+
+var temporaryFolder = appDataType?.GetRuntimeProperty(propertyName)!.GetValue(CurrentApplicationData);
+
+Console.WriteLine($"temporaryFolder?.GetType(): {temporaryFolder?.GetType()}");
+Console.WriteLine($"temporaryFolder?.GetType().GetRuntimeProperty("Path"): {temporaryFolder?.GetType().GetRuntimeProperty("Path")}");
+Console.WriteLine($"temporaryFolder?.GetType().GetRuntimeProperty("Path")!.GetValue(temporaryFolder): {temporaryFolder?.GetType().GetRuntimeProperty("Path")!.GetValue(temporaryFolder)}");
+
+// Console.WriteLine(ApplicationDataHelper.CurrentApplicationData);
+// Console.WriteLine(ApplicationDataHelper.TemporaryFolderPath);
+// Console.WriteLine(ApplicationDataHelper.LocalFolderPath);
 
